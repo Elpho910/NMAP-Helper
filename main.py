51ef1123682336c2d574dbf8d8bd357d68451ad4
Daniel Elphinstone <daniel@elphonet.com>
2024-04-10 21:49:50 +1000

    Add logic to complete a quick or full scan.  Add beginnings of tkinter GUI

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import nmap
 import pandas as pd
 import netifaces
-import PySimpleGUI as sg
+from tkinter import *
+from tkinter import messagebox
 
+window = Tk()
+window.title("Network Enumerator")
+window.config(padx=20, pady=20)
 
-def scan_network(subnet):
+def scan_network(subnet, scan_type):
+    
+    if scan_type == "Full":
+        scan_args = "-sV"
+    else:
+        scan_args = "-sS -F"
+    
     nm = nmap.PortScanner()
-    nm.scan(hosts=subnet, arguments='-sS -F')
+    nm.scan(hosts=subnet, arguments=scan_args)
     
     columns = ['Host', 'Hostname', 'State', 'Protocol', 'Port', 'Service']
     scan_results_df = pd.DataFrame(columns=columns)
@@ -48,9 +58,12 @@ def get_current_subnet():
 
 def main():
     scanned_subnet = get_current_subnet()
+    subnet_label = Label(text=scanned_subnet)
+    subnet_label.grid(column=1, row=0, padx=10, pady=10)
     print(f"Detected subnet: {scanned_subnet}")
+    scan_type = input("Scan Type, Enter: Quick or Full ")
     print("Scanning... Please Wait...")
-    scan_results_df = scan_network(scanned_subnet)
+    scan_results_df = scan_network(scanned_subnet, scan_type)
     print(scan_results_df)
     
     excel_file_name = 'nmap_scan_results.xlsx'
@@ -60,5 +73,9 @@ def main():
     print(f"Scan complete.  Results saved to {excel_file_name}")
 
 
+
+
 if __name__ == "__main__":
     main()
+    
+window.mainloop()
